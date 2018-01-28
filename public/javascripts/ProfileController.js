35906fc7e5663d93b4463ea44210b5ffe0c470d2
ishan10 <ikpandya10@gmail.com>
2018-01-27 21:00:03 -0800

    #6 Step-1:Basic Info Inserted

--- public/javascripts/ProfileController.js
+++ public/javascripts/ProfileController.js
@@ -8,10 +8,46 @@ athleteApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvi
     }).state('profileForm.basic', {
         url: '/basic',
         templateUrl: '../view/basicInfo.ejs'
+    }).state('profileForm.professional', {
+        url: '/professional',
+        templateUrl: '../view/professionalInfo.ejs'
     })
     $urlRouterProvider.otherwise('/profileForm');
 }]);
 
+
 athleteApp.controller("AthleteController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
-    console.log("inside conntroller");
-}])
+    $scope.profile = {};
+
+    console.log($scope.profile);
+
+    $scope.submitProfile = function () {
+
+        var dateObj = new Date($scope.profile.dob);
+        var month = dateObj.getUTCMonth() + 1;
+        var day = dateObj.getUTCDate();
+        var year = dateObj.getUTCFullYear();
+
+        $scope.profile.dob = year + "/" + month + "/" + day;
+
+        //$scope.profile.dob = new Date(newDate);
+
+        console.log($scope.profile.dob);
+
+        if (typeof $scope.profile.name == 'undefined' || typeof $scope.profile.dob == 'undefined') {
+            alert("Please Enter the mandatory fields and resubmit");
+        }
+        else {
+            $http.post('/createProfile', {
+                data: {
+                    profile: $scope.profile
+                }
+            }).then(function (data) {
+                console.log(data);
+            }, function (error) {
+                console.log("error");
+            })
+        }
+    }
+    
+}]);
